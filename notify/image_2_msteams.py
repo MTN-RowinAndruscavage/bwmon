7a3837945086e78e17eacaff321f291dc7f00985
Rowin Andruscavage <Rowin.Andruscavage1@T-Mobile.com>
2024-03-05 13:47:02 -0800

    Pylint fixes; arm MS Teams notification

--- notify/image_2_msteams.py
+++ notify/image_2_msteams.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 # MS Teams doesn't have a straightforward way to upload images directly
-# Workaround: 
+# Workaround:
 # Upload an image to azure blob storage
 # then post image URL to MS Teams via webhook
 
-import os, sys, datetime
-from pathlib import Path
-## Future work: monitor a directory and post images as they appear
-# import inotify.adapters
-import simplejson as json
+# USAGE:
+# Set up variables in .env
+#
+#   pipenv run image_2_msteams.py ~/Pictures/Screenshots/screenshot.png
+#
+
+import os
+import sys
 import pymsteams
 from azure.storage.blob import BlobServiceClient, BlobType, ContentSettings
+## Future work: monitor a directory and post images as they appear
+# import inotify.adapters
+# from pathlib import Path
 
 connection_string = os.environ.get('AZURE_CONNECTION_STRING')
 container_name = os.environ.get('AZURE_CONTAINER_NAME')
@@ -19,14 +25,14 @@ blob_service_client = BlobServiceClient.from_connection_string(connection_string
 network = os.environ.get('ENV') or "Unset ENV"
 msteams_webhook = os.environ.get('MSTEAMS_WEBHOOK')
 debug = os.environ.get('DEBUG') or False
-image_file = "./5G.png"
 
 def upload_blob_file(imagefile, blob_service_client: BlobServiceClient, container_name: str):
+    """Upload to Azure blob storage"""
     container_client = blob_service_client.get_container_client(container=container_name)
     content_settings=ContentSettings(content_type="image/png")
     with open(file=imagefile, mode="rb") as data:
         blob_client = container_client.upload_blob(
-            name=f"screenshots/{os.path.basename(imagefile)}", 
+            name=f"screenshots/{os.path.basename(imagefile)}",
             data=data,
             blob_type=BlobType.BlockBlob,
             content_settings=content_settings,
@@ -34,24 +40,23 @@ def upload_blob_file(imagefile, blob_service_client: BlobServiceClient, containe
             )
     if '?' in blob_client.url:
         return blob_client.url.split('?')[0]
-    else:
-        return blob_client.url
+    return blob_client.url
+
 
 def msTeamsNotification(image_url):
+    """Post to MS Teams webhook"""
     global msteams_webhook
     msTeamsMsg = pymsteams.connectorcard(msteams_webhook)
     msgSection = pymsteams.cardsection()
     msgSection.addImage(image_url, ititle="Azure Blob")
     msTeamsMsg.addSection(msgSection)
-    msTeamsMsg.title = ('Screenshot')
+    msTeamsMsg.title('Screenshot')
     msTeamsMsg.text('PNG File')
-
     try:
         msTeamsMsg.send()
         assert msTeamsMsg.last_http_response.status_code == 200
     except Exception as e:
         print(f"Error sending to MS Teams: {str(e)}")
-    return
 
 
 # Send notification to MSTeams when screenshot is added
@@ -60,7 +65,7 @@ def _main():
     for imagefile in sys.argv[1:]:
         url = upload_blob_file(imagefile, blob_service_client, container_name)
         print(url)
-        # msTeamsNotification(url)
+        msTeamsNotification(url)
 
     # i = inotify.adapters.Inotify()
 
