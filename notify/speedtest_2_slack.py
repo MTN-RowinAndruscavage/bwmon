52ada2b06178637e4274de01914ab187b5873ade
Rowin Andruscavage <8740187+MTN-RowinAndruscavage@users.noreply.github.com>
2024-03-18 17:23:09 -0700

    Update speedtest_2_slack.py
    Update for selenium 4

--- notify/speedtest_2_slack.py
+++ notify/speedtest_2_slack.py
@@ -2,6 +2,9 @@
 
 from slack_sdk.webhook import WebhookClient
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
 import os, datetime
 from pathlib import Path
 import inotify.adapters
@@ -12,12 +15,13 @@ slack_webhook = os.environ.get('SLACK_WEBHOOK')
 sendNotification = os.environ.get('SEND') or True
 debug = os.environ.get('DEBUG') or False
 openBrowser = os.environ.get('OPENBROWSER') or True
-geckodriver_path = "/snap/bin/geckodriver"
-driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
+# geckodriver_path = "/snap/bin/geckodriver"
+# driver_service = webdriver.FirefoxService(executable_path=geckodriver_path)
 speedtest_file = "/usr/lib/check_mk_agent/plugins/speedtest_data.json"
 
 if openBrowser:
-    browser=webdriver.Firefox(service=driver_service)
+    # browser=webdriver.Firefox(service=driver_service)
+    browser=webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 def readData(filename):
     global debug, network, openBrowser, browser
